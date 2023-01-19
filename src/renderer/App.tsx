d6288bcfe1820d6811258870587a4e6a7923a6ec
Caillou <caioalencarcinel@gmail.com>
2023-01-18 22:07:53 -0300

    preguiça

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,14 +1,10 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
 import 'tailwindcss/tailwind.css';
 
 const Hello = () => {
   return (
     <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1 className="text-xs">electron-react-boilerplate</h1>
+      <h1>Hello World</h1>
     </div>
   );
 };
