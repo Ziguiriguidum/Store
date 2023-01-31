60a7d30dd56656a83d04267125ab20f106cf2fc6
Caillou <caioalencarcinel@gmail.com>
2023-01-31 00:33:03 -0300

    modelo ok

---
+++ .svelte-kit/output/server/manifest-full.js
@@ -0,0 +1,27 @@
+export const manifest = {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.png","robots.txt"]),
+	mimeTypes: {".png":"image/png",".txt":"text/plain"},
+	_: {
+		entry: {"file":"_app/immutable/start-5f4b9cb3.js","imports":["_app/immutable/start-5f4b9cb3.js","_app/immutable/chunks/index-97c4cb29.js","_app/immutable/chunks/singletons-23d89355.js"],"stylesheets":[],"fonts":[]},
+		nodes: [
+			() => import('./nodes/0.js'),
+			() => import('./nodes/1.js'),
+			() => import('./nodes/2.js')
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0], errors: [1], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+};
---
+++ .svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
+export const file = '_app/immutable/components/error.svelte-652bda10.js';
+export const imports = ["_app/immutable/components/error.svelte-652bda10.js","_app/immutable/chunks/index-97c4cb29.js","_app/immutable/chunks/singletons-23d89355.js"];
+export const stylesheets = [];
+export const fonts = [];
