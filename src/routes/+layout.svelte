2d97f0ca7654ac55208d4e091105750ee4b9d50c
Caillou <caioalencarcinel@gmail.com>
2023-03-18 12:00:12 -0300

    home ok

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,7 +47,7 @@
 				<p class="text-xl text-gray-400 font-light pb-2 mb-2 mt-4 border-b border-white border-opacity-30 mx-16">Ziguiriguidum Store</p>
 				<div class="sm:flex flex-wrap mx-16 pb-8 justify-around">
 				{#each games as game}
-					<div>
+					<div class="pb-4 hover:scale-125 hover:text-white brightness-90 hover:brightness-110 cursor-pointer transition p-2 hover:bg-[#252525] hover:z-50 rounded">
 						<img width="256px" src={`https://cdn.akamai.steamstatic.com/steam/apps/${game.id}/header.jpg`} alt="icon" />
 						<p class="text-gray-400 text-xs w-64 whitespace-nowrap text-ellipsis">{game.name}{game.version !== null ? ' - '+game.version.substring(0, 10).replace(/\.$/, "...") : ''} - {Number(game.size).toLocaleString()}GB</p>
 					</div>
