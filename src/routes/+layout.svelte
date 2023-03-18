e8b0d3d5a5d4450f38fac324f9d33d766f903d86
Caillou <caioalencarcinel@gmail.com>
2023-03-18 09:08:55 -0300

    Typo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,14 +11,14 @@
 <div class="app">
 	<WindowMenu />	
 	<div class="flex h-[97vh]">
-		<div class="w-2/12 h-full border-r border-white border-opacity-30 ">
+		<div class="w-2/12 border-r border-white border-opacity-30">
 			<p class="mt-2 text-sm text-center text-gray-400">Your Library</p>
 			<hr class="opacity-50 my-2"/>						
 			{#await store.get("mygames")}
 				<p class="text-sm text-center text-gray-400">Loading...</p>
 			{:then games}
 				{#if !games || games.length === 0}
-					<p class="text-sm text-center text-gray-400">No games</p>
+					<p class="text-sm text-center text-gray-400">No Games Found</p>
 				{:else}
 					{#each games as game}
 					<div class="flex flex-row items-center justify-between px-2 py-1 hover:bg-white hover:bg-opacity-10">
