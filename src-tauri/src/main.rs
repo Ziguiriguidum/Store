6a0e07c05bb90869189b24d463650fd058cce060
caiocinel <caioalencarcinel@gmail.com>
2023-06-16 09:16:15 -0300

    fix slow window move

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,40 +1,37 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 use tauri::{Manager, Size};
-use window_vibrancy::{apply_blur};
-use window_shadows::set_shadow;
 use tauri_plugin_store::StoreBuilder;
-
+use window_shadows::set_shadow;
 
 fn main() {
-  tauri::Builder::default()
-    .plugin(tauri_plugin_store::Builder::default().build())
-    .plugin(tauri_plugin_window_state::Builder::default().build())
-    .invoke_handler(tauri::generate_handler![my_custom_command])
-    .setup(|app| {
-      let window = app.get_window("main").unwrap();
-      let mut _store = StoreBuilder::new(app.handle(), "path/to/store.bin".parse()?).build();
+    tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .invoke_handler(tauri::generate_handler![my_custom_command])
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            let mut _store = StoreBuilder::new(app.handle(), "path/to/store.bin".parse()?).build();
 
-      window.set_size(Size::Logical(tauri::LogicalSize { width: 1280.0, height: 800.0 })).unwrap();
+            window
+                .set_size(Size::Logical(tauri::LogicalSize {
+                    width: 1280.0,
+                    height: 800.0,
+                }))
+                .unwrap();
 
-      #[cfg(target_os = "windows")]
-      apply_blur(&window, Some((18, 18, 18, 150)))
-        .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+            set_shadow(&window, true).unwrap();
 
-      set_shadow(&window, true).unwrap();
-
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
-
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
-
 
 #[tauri::command]
 fn my_custom_command() {
-  println!("I was invoked from JS!");
+    println!("I was invoked from JS!");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script>
 	import Home from '$components/Pages/Home.svelte';
-    import Install from '$components/Modal/install.svelte';
-    
-    $: page = Home;
-    $: props = {};
-    
+	import Install from '$components/Modal/install.svelte';
+
+	$: page = Home;
+	$: props = {};
 </script>
 
-<svelte:component bind:props={props} bind:page={page} this={page}></svelte:component>
-<Install bind:props={props} />
+<svelte:component this={page} bind:props bind:page />
+<Install bind:props />
