dc0d0a318d2dfcc477e6d6e423ec90b85c52385b
Caillou <caioalencarcinel@gmail.com>
2023-10-30 20:08:33 -0300

    fixed background // broken js

--- src/components/Application/WindowMenu.svelte
+++ src/components/Application/WindowMenu.svelte
@@ -1,12 +1,12 @@
 <script>
-    import { appWindow } from '@tauri-apps/api/window';
+
 </script>
 
 <div data-tauri-drag-region class="h-6 select-none flex justify-end fixed top-0 right-0 left-0 bg-black bg-opacity-50 border-b border-white border-opacity-30 z-50">
-    <button on:click={() => appWindow.minimize()} class="w-8 hover:bg-white hover:bg-opacity-30 brightness-125">
+    <button class="w-8 hover:bg-white hover:bg-opacity-30 brightness-125">
         <img src="https://api.iconify.design/mdi:window-minimize.svg" class="invert mx-auto" alt="minimize" />
     </button>
-    <button on:click={() => appWindow.close()} class="w-8 hover:bg-red-500 hover:bg-opacity-70">
+    <button class="w-8 hover:bg-red-500 hover:bg-opacity-70">
         <img src="https://api.iconify.design/mdi:close.svg" class="invert mx-auto" alt="close" />
     </button>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -45,4 +45,5 @@
 			<slot />
 		</div>
 	</div>
+	
 </div>
